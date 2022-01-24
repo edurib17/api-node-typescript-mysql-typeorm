6dee635cb1398724db850e469339ca760dbadcba
Eduardo Emanuel <70245046+edurib17@users.noreply.github.com>
2022-01-24 19:21:53 -0300

    find platform  update

--- src/services/UpdateGameService.ts
+++ src/services/UpdateGameService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from "typeorm";
 import { Game } from "../entities/Games";
+import { Platform } from "../entities/Platform";
 
 type GameUpdateRequest = {
   id: string;
@@ -12,10 +13,13 @@ type GameUpdateRequest = {
 export class UpdateGameService {
   async execute({id,name,description,price,platform_id}: GameUpdateRequest) {
     const repo = getRepository(Game);
+    const repoPlatform = getRepository(Platform);
 
     const game = await repo.findOne(id);
 
     if (!game) return new Error("Game does not exists!!");
+    
+    if (!(await repoPlatform.findOne(platform_id))) return new Error("Platform does not exists");
 
     game.name = name ? name : game.name;
     game.description = description ? description : game.description;
